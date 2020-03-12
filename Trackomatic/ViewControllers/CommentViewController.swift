1375be5bef3b69c1a5e31a2c3cc05e80e0b16053
Tom Cowland <info@tomcowland.com>
2020-03-12 09:44:21 +0000

    Adjust comment height based on content

--- Trackomatic/ViewControllers/CommentViewController.swift
+++ Trackomatic/ViewControllers/CommentViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class CommentViewController: NSViewController
+class CommentViewController: NSViewController, NSTextDelegate
 {
     enum Mode {
         case new;
@@ -19,7 +19,11 @@ class CommentViewController: NSViewController
     
     @objc dynamic var comment: Comment? {
         willSet { willChangeValue(forKey: "editable"); willChangeValue( forKey: "deletable" ); }
-        didSet { didChangeValue( forKey: "editable"); didChangeValue( forKey: "deletable" ); }
+        didSet {
+            didChangeValue( forKey: "editable");
+            didChangeValue( forKey: "deletable" );
+            updateHeight();
+        }
     };
     
     @objc dynamic var manager: CommentManager? {
@@ -35,10 +39,33 @@ class CommentViewController: NSViewController
         return editable && mode != .new;
     }
     
+    @IBOutlet var textView: NSTextView!
+    @IBOutlet weak var textViewHeightContstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        updateHeight();
+    }
+    
     @IBAction func delete( _ sender: Any )
     {
         guard let c = comment else { return };
         comment = nil;
         manager?.remove(comments: [ c ] );
+    }
+    
+    private func updateHeight()
+    {
+        if !isViewLoaded { return; }
+        
+        guard let lm = textView.layoutManager else { return; }
+        
+        lm.ensureLayout( for: textView.textContainer! );
+        let h: CGFloat = max( 40, lm.usedRect( for: textView.textContainer! ).height );
+        textViewHeightContstraint.constant = h;
+    }
+    
+    func textDidChange(_ notification: Notification)
+    {
+        updateHeight();
     }
 }
